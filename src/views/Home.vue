b06a49e369e52a92a3d9d75f606cd9de5e6d78fc
Takeru Furuichi <takf@furuichihotakanoMacBook-Air.local>
2019-04-27 16:15:24 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>{{greetText}}</p>
+    <p>挨拶した回数　：　{{count}}回</p>
+    <p v-if="isRegulars">いつもありがとうございます</p>
+    <p>
+      <MyButton :greet="greetText" @click="onMyButtonClick">挨拶する</MyButton>
+    </p>
+    <p>
+      <ResetButton v-model="greetText"></ResetButton>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Watch, Vue } from 'vue-property-decorator';
+import MyButton from "@/components/MyButton.vue"
+import ResetButton from "@/components/ResetButton.vue" // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    ResetButton,
+    MyButton,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private count: number = 0;
+  public greetText: string = "Hello";
+
+  public get isRegulars(): boolean {
+    return this.count >= 5;
+  }
+
+  @Watch('count')
+  public countChanged(){
+    if(this.count === 5){
+      alert("常連になりました")
+    }
+  }
+
+  public onMyButtonClick(count: number){
+    this.count = count;
+    this.greetText = "こんにちは"
+  }
+}
 </script>
